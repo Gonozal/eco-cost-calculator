82a3526d78f0035077c02c3c0c6fb30aff3b167a
Sören Link <sl@smart-data-deutschland.de>
2022-02-28 17:48:58 +0100

    fix(ui): fix page title

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,6 @@ enableMapSet();
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
       <App />
     </ThemeProvider>
