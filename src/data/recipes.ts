664bd6490053e64bbcb09ead7ee13cb22579fa17
Sören Link <311312+Gonozal@users.noreply.github.com>
2024-01-30 12:53:13 +0100

    fix: fix primary product and byproduct switched for some products

--- src/data/recipes.ts
+++ src/data/recipes.ts
@@ -45,16 +45,32 @@ export const recipes: Recipe[] = recipesFromJson(protoRecipe);
 
 export function recipesFromJson(json: ProtoRecipe) {
   return json.map((recipe) => {
-    const mainProduct = getMainProduct(recipe.products);
-    const byproduct = getByproduct(recipe.products);
+    const mainProduct = getMainProduct(recipe.products, recipe.name);
+    const byproduct = getByproduct(recipe.products, mainProduct);
+
+    if (recipe.name === 'Crispy Bacon')
+      console.log({ recipeName: recipe.name, mainProduct, byproduct });
 
     return { ...recipe, mainProduct, byproduct };
   });
 }
 
-export function getMainProduct(products: Product[]): Product {
+export function getMainProduct(
+  products: Product[],
+  recipeName: string,
+): Product {
   // Usual case. Only 1 product
   if (products.length === 1) return products[0];
+
+  // https://github.com/Gonozal/eco-cost-calculator/issues/3
+  const identicallyNamedProduct = products.find((product) => {
+    const concatRecipeName = recipeName.replace(' ', '');
+    const match = product.name.match(concatRecipeName);
+
+    return match;
+  });
+
+  if (identicallyNamedProduct) return identicallyNamedProduct;
 
   // "Waste" Byproduct that scales with inputs. E.g. smelting (slag) or concentrating (tailings), oil drilling (barrels)
   const hasScalingProduct = products.some((product) => !product.isConstant);
@@ -67,14 +83,13 @@ export function getMainProduct(products: Product[]): Product {
   // Are there other cases?
 }
 
-export function getByproduct(products: Product[]): Product | undefined {
+export function getByproduct(
+  products: Product[],
+  mainProduct: Product,
+): Product | undefined {
   // Usual case. Only 1 main product
   if (products.length === 1) return undefined;
 
   // "Waste" Byproduct that scales with inputs. E.g. smelting (slag) or concentrating (tailings), oil drilling (barrels)
-  const scalingProduct = products.find((product) => !product.isConstant);
-  if (scalingProduct) return scalingProduct;
-
-  // Return The product with the smallest quantity (e.g. ore crushing)
-  return [...products].sort((a, b) => a.quantity - b.quantity)[0];
+  return products.filter((product) => product.name !== mainProduct.name)[0];
 }
